56da884983762060407b7cf1362573d3dabb86a0
Arjan Jakupi <arjan.jakupi007@gmail.com>
2023-10-21 11:39:18 +0200

    navbar basic funct

--- convex/documents.ts
+++ convex/documents.ts
@@ -229,3 +229,63 @@ export const getSearch = query({
         return documents;
     }
 });
+
+export const getById = query({
+    args: { documentId: v.id("documents")},
+    handler: async (ctx, args) => {
+        const identiy = await ctx.auth.getUserIdentity();
+
+        const document = await ctx.db.get(args.documentId);
+
+        if(!document)
+            throw new Error("Not found");
+
+        if(document.isPublished && !document.isArchived)
+            return document;
+
+        if(!identiy)
+            throw new Error("Not authenticated");
+
+        const userId = identiy.subject;
+
+        if(document.userId !== userId)
+            throw new Error("Unauthorized");
+
+        return document;
+    }
+});
+
+export const update = mutation({
+    args: {
+        id: v.id("documents"),
+        title: v.optional(v.string()),
+        content: v.optional(v.string()),
+        coverImage: v.optional(v.string()),
+        icon: v.optional(v.string()),
+        isPublished: v.optional(v.boolean())
+    },
+    handler: async (ctx, args) => {
+        const identiy = await ctx.auth.getUserIdentity();
+
+        if(!identiy)
+            throw new Error("Unauthenticated");
+
+        const userId = identiy.subject;
+
+        const { id, ...rest} = args;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if(!existingDocument)
+            throw new Error("Not found");
+
+        if(existingDocument.userId !== userId)
+            throw new Error("Unauthroized");
+
+        const document = await ctx.db.patch(args.id, {
+            ...rest,
+        });
+
+        return document;
+    }
+});
