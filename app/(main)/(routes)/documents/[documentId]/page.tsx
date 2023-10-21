7feb2e631d3deea16732900165b523aea55d2eae
Arjan Jakupi <arjan.jakupi007@gmail.com>
2023-10-21 16:40:42 +0200

    editor functionality

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -2,10 +2,11 @@
 
 import { Toolbar } from "@/app/(main)/_components/toolbar";
 import { Cover } from "@/components/cover";
+import { Editor } from "@/components/editor";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdPageProps {
     params: {
@@ -17,6 +18,15 @@ const DocumentIdPage = ({params} : DocumentIdPageProps) => {
     const document = useQuery(api.documents.getById, {
         documentId: params.documentId
     });
+
+    const update = useMutation(api.documents.update);
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content
+        });
+    };
 
     if(document === undefined){
         return (
@@ -47,6 +57,9 @@ const DocumentIdPage = ({params} : DocumentIdPageProps) => {
             <Cover url={document.coverImage}/>
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document}/>
+                <Editor
+                 onChange={onChange}
+                 initialContent={document.content}/>
             </div>
         </div>
     );
